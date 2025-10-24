a24359048b1984bf010208b93e3dbffef26f0225
hedfiJinene <jinenehedfi9@gmail.com>
2025-10-24 21:04:21 +0100

    ajout des requetes API

--- src/test/java/com/jinene/api/IpstakApiTest.java
+++ src/test/java/com/jinene/api/IpstakApiTest.java
@@ -30,6 +30,43 @@ public class IpstakApiTest {
         Assert.assertEquals(response.statusCode(), 200);
         Assert.assertEquals(response.jsonPath().getString("ip"), ip);
         Assert.assertNotNull(response.jsonPath().getString("country_name"));
-    }
+    } @Test
+    public void testIpGeolocation_InvalidAccessKey() {
+        String invalidKey = "INVALID_KEY";
+        String ip = "134.201.250.155";
 
+        Response response = RestAssured
+                .given()
+                .queryParam("access_key", invalidKey)
+                .when()
+                .get("/" + ip)
+                .then()
+                .extract().response();
+
+        System.out.println("=== Réponse clé API invalide ===");
+        System.out.println(response.asPrettyString());
+
+        Assert.assertEquals(response.statusCode(), 200, "Même avec une clé invalide, le code est souvent 200");
+        Assert.assertTrue(response.asString().contains("error"), "La réponse doit contenir un message d’erreur");
+    }
+    @Test
+    public void testIpGeolocation_InvalidIpAddress() {
+    	String accessKey = "29e3eb616a16193e8777df6f80f84cc2";
+        String invalidIp = "999.999.999.999";
+
+        Response response = RestAssured
+                .given()
+                .queryParam("access_key", accessKey)
+                .when()
+                .get("/" + invalidIp)
+                .then()
+                .extract().response();
+
+        System.out.println("=== Réponse IP invalide ===");
+        System.out.println(response.asPrettyString());
+
+        Assert.assertEquals(response.statusCode(), 200, "Le code HTTP doit être 200 même pour une IP invalide");
+        Assert.assertTrue(response.asString().contains("error") || response.jsonPath().getString("ip") == null,
+                "La réponse doit contenir une erreur ou une IP nulle");
+    }
 }
